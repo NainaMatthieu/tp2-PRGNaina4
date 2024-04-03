653e2687308cb78eb232411bb4a7ec31d3d3ae3a
NainaMatthieu <andrianmattax@gmail.com>
2024-04-03 19:51:53 +0300

    Création du projet

---
+++ src/main/java/mg/itu/tp2/prgnaina4/config/ConfigJSF.java
@@ -0,0 +1,18 @@
+/*
+*Classe de configuration pour JavaServer Faces (JSF).
+*Cette classe est responsable de la configuration de l'application JSF.
+*/
+package mg.itu.tp2.prgnaina4.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ *
+ * @author NainaMatthieu
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+
+}
