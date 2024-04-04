2da0cc53a76139d80c11280e5fe07f0ce7d0d6c9
NainaMatthieu <andrianmattax@gmail.com>
2024-04-04 19:04:07 +0300

    Exercice 4

---
+++ src/main/java/mg/itu/tp2/prgnaina4/jsf/Bean1.java
@@ -0,0 +1,44 @@
+/**
+ * Backing bean pour les page JSF  pour effectuer des opérations sur les nombres.
+ *Portée : request
+ *
+ * @author NainaMatthieu
+ */
+package mg.itu.tp2.prgnaina4.jsf;
+
+import java.util.ArrayList;
+import java.util.List;
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+
+@Named(value = "bean1")
+@RequestScoped
+public class Bean1 {
+
+    private int nombre;
+
+    public int getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(int nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Integer> getNombresSuivants() {
+        int nb = 5;
+        List<Integer> l = new ArrayList<>(nb);
+        for (int i = nombre; i < nombre + nb; i++) {
+            l.add(i);
+        }
+        return l;
+    }
+
+    public String afficher() {
+        return "affichage_4?nb=" + nombre + "&;faces-redirect=true";
+    }
+
+    public String retourFormulaire() {
+        return "formulaire_4?nb=" + nombre + "&;faces-redirect=true";
+    }
+}
